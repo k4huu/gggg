ee7a9364f72dabae6529d178b006651ccd39b923
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2022-08-15 12:05:09 +0200

    transaction date & date sorting

--- pages/panel/shop/_shopid/index.vue
+++ pages/panel/shop/_shopid/index.vue
@@ -32,6 +32,9 @@
         :headers="headers"
         :items="transactions"
         :search="search"
+        :custom-sort="customSort"
+        :sort-by="'date'"
+        :sort-desc="false"
       />
     </v-card>
   </div>
@@ -56,7 +59,8 @@ export default {
       headers: [
         { text: 'Id', value: 'id' },
         { text: 'Kupujący', value: 'nick' },
-        { text: 'Brama', value: 'type' }
+        { text: 'Brama', value: 'type' },
+        { text: 'Wykonanie', value: 'date' }
       ]
     }
   },
@@ -69,13 +73,49 @@ export default {
     transactions () {
       const result = []
       for (const i in this.shop.history) {
+        const date = new Date(this.shop.history[i].date).toLocaleString('pl-PL')
         result.push({
           id: i,
           nick: this.shop.history[i].nick,
-          type: this.shop.history[i].type
+          type: this.shop.history[i].type,
+          date
         })
       }
       return result
+    }
+  },
+  methods: {
+    localeStringToNumber (str) {
+      let [p1, p2] = str.split(', ')
+      p1 = p1.split('.')
+      p2 = p2.split(':')
+      return (new Date(parseInt(p1[2]), parseInt(p1[1]), parseInt(p1[0]), parseInt(p2[0]), parseInt(p2[1]), parseInt(p2[2]))).getTime()
+    },
+    customSort (items, index, isDesc) {
+      items.sort((a, b) => {
+        if (index[0] == 'date') {
+          if (!isDesc[0]) {
+            return this.localeStringToNumber(b[index]) - this.localeStringToNumber(a[index])
+          } else {
+            return this.localeStringToNumber(a[index]) - this.localeStringToNumber(b[index])
+          }
+        } else if (isDesc[0]) {
+          if (a[index] > b[index]) {
+            return -1
+          } else if (a[index] < b[index]) {
+            return 1
+          } else {
+            return 0
+          }
+        } else if (a[index] < b[index]) {
+          return -1
+        } else if (a[index] > b[index]) {
+          return 1
+        } else {
+          return 0
+        }
+      })
+      return items
     }
   }
 }
